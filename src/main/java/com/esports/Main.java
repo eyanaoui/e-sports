31f2cb9a9615e948f960705a3a7662c87170fc67
eyanaoui <naouieya22@gmail.com>
2026-03-16 17:24:28 +0100

    feat: Game CRUD, DAO, controller, FXML view

--- src/main/java/com/esports/Main.java
+++ src/main/java/com/esports/Main.java
@@ -1,9 +1,22 @@
 package com.esports;
 
-import com.esports.db.DatabaseConnection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/game-view.fxml   replace it with ur own fxml file to test your work else u can test mine"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Esports Platform");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        DatabaseConnection.getInstance();
+        launch(args);
     }
 }
